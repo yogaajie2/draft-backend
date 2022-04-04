c4304b9f11aebd84f8baa6da8f9d0e643f11c9f5
yogaajie2 <yogaajie2@gmail.com>
2022-04-04 11:42:38 +0700

    build: update provider config

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: env('AKIAY7IUBKKA7W6KQAES'),
-      secretAccessKey: env('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'),
-      region: 'us-east-1',
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
       params: {
-        Bucket: 'draft-uploads',
+        Bucket: env('AWS_BUCKET'),
       },
     },
   },
